a65a01deceae41fe328a9c9823a68a9966d2d8d1
Hakan <143508175+KebanFiru@users.noreply.github.com>
2024-12-22 15:01:19 +0300

    System that print out usernames is changed

--- Year #1/Homework #3/src/main.py
+++ Year #1/Homework #3/src/main.py
@@ -1,19 +1,29 @@
-def AddUser():
+def UserManagement():
     User = []
+    UserList = []
+    NewUser = []
 
     with open(r"input\users.txt", "r") as file: 
         #Open file for read
 
         lines = file.readlines()
-        print("Existin users:")
-        for i in range(len(lines)):
-        #subtract "\n" from elements of lines and print it out
-            if(i!=0):
-            #Thanks to it the first line won't write on the screen
+        print("Existing users:")
 
-                
-                print(lines[i][0].strip())
-                #Because of the fact that I added users as a list element I can use list[i][0]
+        for i in range(len(lines)):
+        #Open a for loop for add items of the file to a 2d array
+
+                NewUser = [lines[i]]
+                UserList.append(NewUser)
+
+        for i in range(len(UserList)):
+
+            for k in range( len(UserList[i])):
+
+                if k%3 == 0:
+                #It will append items that can be divised to 3 
+
+                    print(UserList[i][k].split(',')[0])
+                    #Print the items before first comma
 
     Username = input("Name(yourname_surname):")
 
@@ -32,8 +42,6 @@ def AddUser():
 
         file.writelines(User)
         #Add "\n" 
- 
-def Login():
 
     UserName = input("Username(for login):")
 
@@ -45,4 +53,13 @@ def Login():
         #Check if UserName + "|n" is in the list
 
             print(f"Login succesfull. Welcome {UserName}")
+
+def ProductManagement():
+    with open(r"input\products.txt", "r") as file:
+        list = file.readlines()
+
+        for i in range(len(list)):
+
+            if(i!=0):
+                print(f"{i}. item, {list[i][0]}, {list[i][1]}, {list[i][2]}" )
 
