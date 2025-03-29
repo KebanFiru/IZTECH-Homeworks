770477039ff9e8433e66265194e4a60aa08725b2
Denzege-ozcn <denizegeozcan2@gmail.com>
2025-03-29 21:51:13 +0300

    Add files via upload

---
+++ Year #1/Second Half/Homework #1/IBag.java
@@ -0,0 +1,26 @@
+public interface IBag<T> {
+
+    public boolean add(T newEntry);
+
+    public boolean isEmpty();
+
+    public boolean isFull();
+
+    public T removeByIndex(int index);
+
+    public int getFrequencyOf(T anEntry);
+
+    public int getIndexOf(T anEntry);
+
+    public boolean contains(T anEntry);
+
+    public void clear();
+
+    public void displayItems();
+
+    public int getCurrentSize();
+
+    public T[] toArray();
+
+
+}
