255ce1ec2029209e938fedd7e558094d61b1868a
Hakan <143508175+KebanFiru@users.noreply.github.com>
2025-04-02 18:34:55 +0300

    GameApp

--- Year #1/Second Half/Homework #1/GameApp.java
+++ Year #1/Second Half/Homework #1/GameApp.java
@@ -2,7 +2,12 @@ public class GameApp {
 
     public static void main(String[] args){
 
+        Player player = new Player("Player");
+        Box box = new Box();
+        Chest chest = new Chest();
+        Game game = new Game(5, player, box,  chest);
 
+        game.play();
 
     }
 
