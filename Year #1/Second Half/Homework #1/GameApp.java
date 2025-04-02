09eb7776e0cc0ca2cdeefcb1e55e974151dcae98
Denzege-ozcn <denizegeozcan2@gmail.com>
2025-04-02 21:48:34 +0300

    Update GameApp.java

--- Year #1/Second Half/Homework #1/GameApp.java
+++ Year #1/Second Half/Homework #1/GameApp.java
@@ -1,14 +1,21 @@
 public class GameApp {
 
     public static void main(String[] args){
-
-        Player player = new Player("Player");
+        Player player = new Player("Adventurer");
         Box box = new Box();
         Chest chest = new Chest();
-        Game game = new Game(5, player, box,  chest);
+
+        Game game = new Game(5, player, box, chest);
 
         game.play();
 
-    }
+        System.out.println("\n\nRunning the game again...\n");
 
+        player = new Player("Adventurer");
+        box = new Box();
+        chest = new Chest();
+        game = new Game(5, player, box, chest);
+        game.play();
+    }
 }
+
