b0b7136db439accbb1901de690e451a8a989b144
Hakan <143508175+KebanFiru@users.noreply.github.com>
2025-06-07 11:47:45 +0300

    Deque is added

--- Year #1/Second Half/Homework #2/Deque.java
+++ Year #1/Second Half/Homework #2/Deque.java
@@ -1,2 +1,19 @@
-public interface Deque {
+public interface Deque <T>{
+
+    void addFront(T data);
+
+    void addRear(T data);
+
+    T removeFront();
+
+    T removeRear();
+
+    T peekFront();
+
+    T peekRear();
+
+    boolean isEmpty();
+
+    int size();
+
 }
--- Year #1/Second Half/Homework #2/GenericDeque.java
+++ Year #1/Second Half/Homework #2/GenericDeque.java
@@ -1,2 +1,143 @@
-public class GenericDeque {
+public class GenericDeque<T> implements Deque<T> {
+
+    private Node<T> front;
+    private Node<T> rear;
+    private int size;
+
+    public GenericDeque(){
+
+        this.front = null;
+        this.rear = null;
+        size = 0;
+
+    }
+
+    public void addFront(T data){
+
+        Node<T> newNode = new Node<>(data);
+
+        if(isEmpty()){
+
+            front = rear = newNode;
+
+        }
+
+        else{
+
+            newNode.next = front;
+            front.prev = newNode;
+            front = newNode;
+
+        }
+
+        size++;
+
+    }
+
+    public void addRear(T data){
+
+        Node<T> newNode = new Node<>(data);
+
+        if(isEmpty()){
+
+            front = rear = newNode;
+
+        }
+
+        else{
+
+            rear.next = newNode;
+            newNode.prev = rear;
+            rear = newNode;
+
+        }
+
+        size++;
+
+    }
+
+    public T removeFront(){
+
+        if(isEmpty()){
+
+            throw new RuntimeException("Deque is empty");
+
+        }
+
+        T removed = front.data;
+        front = front.next;
+        if(front == null){
+
+            rear = null;
+
+        }
+        else{
+
+            front.prev = null;
+
+        }
+        size--;
+        return removed;
+
+    }
+
+    public T removeRear(){
+
+        if(isEmpty()){
+
+            throw new RuntimeException("Deque is empty");
+
+        }
+
+        T removed = rear.data;
+        rear = rear.prev;
+        if(rear == null){
+
+            front = null;
+
+        }
+        else{
+
+            rear.next = null;
+
+        }
+        size--;
+        return removed;
+
+    }
+
+    public T peekFront(){
+
+        if(isEmpty()){
+
+            throw new RuntimeException("Deque is empty");
+
+        }
+        return front.data;
+
+    }
+
+    public T peekRear(){
+
+        if(isEmpty()){
+
+            throw new RuntimeException("Deque is empty");
+
+        }
+        return rear.data;
+
+    }
+
+    public boolean isEmpty(){
+
+        return front == null;
+
+    }
+
+    public int size(){
+
+        return size;
+
+    }
+
 }
