2d7d78ebd8d77e4b54bbd432fa6988bad755e84c
KebanFiru <hbagic@makersan.com>
2025-10-23 21:25:53 +0300

    Initial commit for the class

---
+++ Year #2/First Half/CENG 211/The E-Sports Tourment Challenge/src/PointsBoard.java
@@ -0,0 +1,20 @@
+public class PointsBoard {
+
+    private Gamer[] gamers = new Gamer[];
+
+    public PointsBoard(Gamer[] gamersArray){
+
+        gamersArray = this.gamers;
+
+    }
+
+    public calculateTotalPoints(){
+
+    }
+    public calculateAvaragePointsPerMatch(){
+
+    }
+    public findMedal(){
+
+    }
+}
