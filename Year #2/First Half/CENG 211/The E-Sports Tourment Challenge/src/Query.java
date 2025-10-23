e279757a62a6c724fbf43f3d6f2699c2815e7eac
KebanFiru <hbagic@makersan.com>
2025-10-24 00:14:23 +0300

    Initial commit

---
+++ Year #2/First Half/CENG 211/The E-Sports Tourment Challenge/src/Query.java
@@ -0,0 +1,15 @@
+public class Query {
+
+    private int[] totalPoints;
+    private Match[][] matches
+
+    public Query(int[] totalPointsArray, Match[][] playedMatches){
+
+        this.totalPoints = totalPointsArray;
+        this.matches = playedMatches;
+    }
+
+    public int findHighestScoringMatch() {
+
+    }
+}
