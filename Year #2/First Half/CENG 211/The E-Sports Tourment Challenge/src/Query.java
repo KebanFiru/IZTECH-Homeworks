1eac08796d96f1921b44a83fd92dd352b157c868
KebanFiru <hbagic@makersan.com>
2025-10-26 14:10:15 +0300

    Constructor is updated and getter method for higher score match is added

--- Year #2/First Half/CENG 211/The E-Sports Tourment Challenge/src/Query.java
+++ Year #2/First Half/CENG 211/The E-Sports Tourment Challenge/src/Query.java
@@ -2,22 +2,28 @@ public class Query {
 
     private int[] totalPoints;
     private Match[][] matches
+    private Match highestScoringMatch;
 
     public Query(int[] totalPointsArray, Match[][] playedMatches){
 
         this.totalPoints = totalPointsArray;
         this.matches = playedMatches;
+
+        findHighestScoringMatch();
     }
 
-    public int findHighestScoringMatch() {
-        Match highestScoringMatch = null;
+    public Match getHighestScoringMatch(){
+
+        return highestScoringMatch;
+    }
+
+    public void findHighestScoringMatch() {
         for(int i =0; i< matches.length; i++){
             for(int j=0; j<matches[i].length; j++){
-                if(matches[i][j] > highestScoringMatch){
+                if(matches[i][j].getMatchPoints > highestScoringMatch){
                     highestScoringMatch = matches[i][j];
                 }
             }
         }
-        return highestScoringMatch;
     }
 }
