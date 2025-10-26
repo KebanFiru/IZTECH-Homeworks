ecc075b4a8ff510d0b698480c6f19a52a743074a
KebanFiru <hbagic@makersan.com>
2025-10-26 14:23:19 +0300

    Find ID of highes scoring match is completed

--- Year #2/First Half/CENG 211/The E-Sports Tourment Challenge/src/Query.java
+++ Year #2/First Half/CENG 211/The E-Sports Tourment Challenge/src/Query.java
@@ -1,27 +1,33 @@
 public class Query {
 
     private int[] totalPoints;
-    private Match[][] matches
-    private Match highestScoringMatch;
+    private Match[][] matches;
+
+    private int highestScoringMatchID;
 
     public Query(int[] totalPointsArray, Match[][] playedMatches){
 
         this.totalPoints = totalPointsArray;
         this.matches = playedMatches;
 
+        this.highestScoringMatchID = 0;
+
         findHighestScoringMatch();
     }
 
-    public Match getHighestScoringMatch(){
+    public int getHighestScoringMatch(){
 
-        return highestScoringMatch;
+        return highestScoringMatchID;
     }
 
     public void findHighestScoringMatch() {
-        for(int i =0; i< matches.length; i++){
-            for(int j=0; j<matches[i].length; j++){
-                if(matches[i][j].getMatchPoints > highestScoringMatch){
-                    highestScoringMatch = matches[i][j];
+        int highestScoringMatchPoint = matches[0][0].getMatchPoint;
+
+        for(int i =0; i< matches.length; i++) {
+            for (int j = 0; j < matches[i].length; j++) {
+                if (matches[i][j].getMatchPoint > highestScoringMatchPoint) {
+                    highestScoringMatchPoint = matches[i][j].getMatchPoint;
+                    highestScoringMatchID = 15*(i-1)+j;
                 }
             }
         }
