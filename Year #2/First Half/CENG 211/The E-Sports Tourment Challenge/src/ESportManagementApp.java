47c29b75701235350d4e6a9206bb070ed671d738
KebanFiru <hbagic@makersan.com>
2025-11-02 00:13:03 +0300

    Initial commit

--- Year #2/First Half/CENG 211/The E-Sports Tourment Challenge/src/ESportManagementApp.java
+++ Year #2/First Half/CENG 211/The E-Sports Tourment Challenge/src/ESportManagementApp.java
@@ -1,13 +1,7 @@
 public class ESportManagementApp {
+
+    
     public static void main(String[] args) {
-        /**
-         *TODO: Use FileIO here for add Array of gamers into PointsBoard class
-         */
-        int totalGamesPlayed = 0;
-        if(totalGamesPlayed == 1500){
-            /**
-             * TODO: add logic here for PointsBoard
-             * */
         }
 
     }
