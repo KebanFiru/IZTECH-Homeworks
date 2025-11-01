349fe7a76939d06af69337c224aeda2dd41376d2
KebanFiru <hbagic@makersan.com>
2025-11-02 02:16:11 +0300

    ESportManagementApp.java is documentaized

--- Year #2/First Half/CENG 211/The E-Sports Tourment Challenge/src/ESportManagementApp.java
+++ Year #2/First Half/CENG 211/The E-Sports Tourment Challenge/src/ESportManagementApp.java
@@ -1,29 +1,33 @@
 public class ESportManagementApp {
 
-    private static final String GAMES_CSV = "games.csv";
-    private static final String GAMERS_CSV = "gamers.csv";
-    private static final int MATCHES_PER_GAMER = 15;
+    /**
+     * Add relative paths of files as static final variables for increase readibility
+     * */
+    private static final String GAMES_CSV = "files/games.csv";
+    private static final String GAMERS_CSV = "files/gamers.csv";
 
     public static void main(String[] args) {
 
+    /*-------------Define Instances of Necesseray Classes-------------*/
         Game[] games = FileIO.readGames(GAMES_CSV);
         Gamer[] gamers = FileIO.readGamers(GAMERS_CSV);
 
-        System.out.println(GAMERS_CSV);
-
         MatchManagement matchManagement = new MatchManagement(gamers,games);
 
-        matchManagement.generateMatches();
+        matchManagement.generateMatches();/*This is for generate matches*/
         PointsBoard pointsBoard = new PointsBoard(gamers, matchManagement.getAllMatches());
 
         Query query = new Query(matchManagement.getAllMatches(), gamers, pointsBoard);
-        Query copyQuery = new Query(query);
+        Query copyQuery = new Query(query);/*I created here a instance of Query with copy constructor for increase safety*/
+    /*----------------------------------------------------------------*/
 
+    /*---------Print Information on Screen Gathered by Query---------*/
         System.out.println(copyQuery.getHighestScoringMatch());
         System.out.println(copyQuery.getLowestScoringMatch());
         System.out.println(copyQuery.getMatchWithTheLowestBonusPoints());
         System.out.println(copyQuery.getHighestScoringGamer());
         System.out.println(copyQuery.getMedalDistribution());
+    /*----------------------------------------------------------------*/
 
 
     }
