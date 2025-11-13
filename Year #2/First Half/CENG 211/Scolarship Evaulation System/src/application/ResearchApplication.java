1763abb4559d10223151ac24f6426f008714e675
KebanFiru <hbagic@makersan.com>
2025-11-13 22:00:08 +0300

    Removed unneceesssary parts

--- Year #2/First Half/CENG 211/Scolarship Evaulation System/src/application/ResearchApplication.java
+++ Year #2/First Half/CENG 211/Scolarship Evaulation System/src/application/ResearchApplication.java
@@ -16,22 +16,7 @@ public class ResearchApplication extends Application {
         StringBuilder result = new StringBuilder();
         result.append("Applicant ID: ").append(applicantID)
                 .append(", Name: ").append(name)
-                .append(", Scholarship: ").append(getScholarshipCategory())
-                .append(", Status: ").append(status);
-
-        if (status.equals("Accepted")) {
-            result.append(", Type: ").append(scholarshipType)
-                    .append(", Duration: ");
-
-            if (durationInYears == 0) {
-                result.append("6 months");
-            } else {
-                result.append(durationInYears)
-                        .append(durationInYears == 1 ? " year" : " years");
-            }
-        } else {
-            result.append(", Reason: ").append(rejectionReason);
-        }
+                .append(", Scholarship: ").append(getScholarshipCategory());
 
         return result.toString();
     }
