da853589583f969b209350594f8867b0c725322c
KebanFiru <hbagic@makersan.com>
2025-11-13 19:31:54 +0300

    Normal and copy constructor is waiting for response

--- Year #2/First Half/CENG 211/Scolarship Evaulation System/src/logic/ScholarshipEvaulator.java
+++ Year #2/First Half/CENG 211/Scolarship Evaulation System/src/logic/ScholarshipEvaulator.java
@@ -1,16 +1,13 @@
 package logic;
 
 import java.util.ArrayList;
+import application.Application;
 
 public class ScholarshipEvaulator {
 
     ArrayList<Application> applications = new ArrayList<>();
 
-    private String applicationStatus;
-    private String applicationDuration;
-
     public ScholarshipEvaulator(ArrayList<Application> allApplications){
-
 
     }
 
@@ -18,40 +15,29 @@ public class ScholarshipEvaulator {
         
     }
 
-    public String getApplicationStatus(Application application){
-        calculateApplicationStatus(application);
-        return;
-    }
-
-    public String getApplicationDuration(Application application){
-        calculateApplicationDuration(application);
-        return;
-    }
 
     public void getApplicants(){
 
         sortApplicants();
         for(Application applicant: applications){
-            applicentData = String.format("Applicant ID: %d, Name; %s, Scholarship:%s, Status: %s, Type:%s,Duration: %s",
-                                                                                                    applicant.getId(),
-                                                                                                    applicant.getName(),
-                                                                                                    applicant.getScholarship(),
-                                                                                                    getApplicationStatus(applicant),
-                                                                                                    applicant.getType(),
-                                                                                                    getApplicationDuration(applicant));
-            System.out.println(applicentData);
+            System.out.println(applicant);
         }
     }
 
-    private void calculateApplicationStatus(Application application){
-
-    }
-
-    private void calculateApplicationDuration(Application application){
-
-    }
 
     private void sortApplicants(){
+        for(int i = 0; i<applications.size(); i++){
+            if(i != applications.size()-1){
+                for(int j=i+1; j<applications.size(); j++){
+                    if(Integer.parseInt(applications.get(j).getApplicantID()) > Integer.parseInt(applications.get(i).getApplicantID())){
+                        Application tempApplication = applications.get(i);
+                        applications.set(i, applications.get(j));
+                        applications.set(j, tempApplication);
 
+                    }
+                }
+            }
+
+        }
     }
 }
