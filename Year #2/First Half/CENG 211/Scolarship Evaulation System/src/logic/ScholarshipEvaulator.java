7f57cb821c2d32b584da44d9c7ab90e104636715
KebanFiru <hbagic@makersan.com>
2025-11-13 22:19:06 +0300

    Refactor commit

--- Year #2/First Half/CENG 211/Scolarship Evaulation System/src/logic/ScholarshipEvaulator.java
+++ Year #2/First Half/CENG 211/Scolarship Evaulation System/src/logic/ScholarshipEvaulator.java
@@ -1,29 +1,57 @@
 package logic;
 
 import java.util.ArrayList;
+
 import application.Application;
 
 public class ScholarshipEvaulator {
 
     ArrayList<Application> applications = new ArrayList<>();
 
+    private String applicationStatus;
+    private String applicationDuration;
+
     public ScholarshipEvaulator(ArrayList<Application> allApplications){
+
 
     }
 
     public ScholarshipEvaulator(ScholarshipEvaulator another){
-        
+
     }
 
+    public String getApplicationStatus(Application application){
+        calculateApplicationStatus(application);
+        return;
+    }
+
+    public String getApplicationDuration(Application application){
+        calculateApplicationDuration(application);
+        return;
+    }
 
     public void getApplicants(){
 
         sortApplicants();
         for(Application applicant: applications){
-            System.out.println(applicant);
+            String applicentData = String.format("Applicant ID: %d, Name; %s, Scholarship:%s, Status: %s, Type:%s,Duration: %s",
+                                        applicant.getApplicantID(),
+                                        applicant.getName(),
+                                        applicant.getScholarshipCategory(),
+                                        getApplicationStatus(applicant),
+                                        applicant.getType(),
+                                        getApplicationDuration(applicant));
+            System.out.println(applicentData);
         }
     }
 
+    private void calculateApplicationStatus(Application application){
+
+    }
+
+    private void calculateApplicationDuration(Application application){
+
+    }
 
     private void sortApplicants(){
         for(int i = 0; i<applications.size(); i++){
@@ -41,3 +69,6 @@ public class ScholarshipEvaulator {
         }
     }
 }
+
+
+
