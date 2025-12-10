b7ecfa6bd00f670b95c1a9765d7ae640c432df79
KebanFiru <hbagic@makersan.com>
2025-12-11 01:42:29 +0300

    Royal penguin is rearranged

--- Year #2/First Half/CENG 211/Sliding Penguins Puzzle Game App/src/penguins/RoyalPenguin.java
+++ Year #2/First Half/CENG 211/Sliding Penguins Puzzle Game App/src/penguins/RoyalPenguin.java
@@ -11,4 +11,17 @@ public class RoyalPenguin extends Penguin{
     public PenguinType getType() {
         return PenguinType.ROYAL;
     }
+    
+    @Override
+    public void useSpecialAbility(Direction dir, IcyTerrain terrain) {
+        int[] pos = getPosition();
+        int[] next = terrain.getNextPosition(pos[0], pos[1], dir);
+        if (terrain.isValidPosition(next[0], next[1])) {
+            terrain.removeObject(pos[0], pos[1]);
+            setPosition(next[0], next[1]);
+            terrain.placeObject(this, next[0], next[1]);
+        }
+        terrain.slidePenguin(this, dir, -1, false); // Then regular slide
+        setSpecialEffectUsed(true);
+    }
 }
