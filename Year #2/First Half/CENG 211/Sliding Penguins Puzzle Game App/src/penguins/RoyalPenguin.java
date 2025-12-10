3ac63dc41de7bbd78d6978ee2e52db9f3fba1826
KebanFiru <hbagic@makersan.com>
2025-12-11 01:00:53 +0300

    Royal penguin is added

--- Year #2/First Half/CENG 211/Sliding Penguins Puzzle Game App/src/penguins/RoyalPenguin.java
+++ Year #2/First Half/CENG 211/Sliding Penguins Puzzle Game App/src/penguins/RoyalPenguin.java
@@ -1,4 +1,14 @@
 package penguins;
 
-public class RoyalPenguin {
+public class RoyalPenguin extends Penguin{
+
+    public RoyalPenguin(String name){
+
+        super(name,PenguinType.ROYAL);
+    }
+
+    @Override
+    public PenguinType getType() {
+        return PenguinType.ROYAL;
+    }
 }
