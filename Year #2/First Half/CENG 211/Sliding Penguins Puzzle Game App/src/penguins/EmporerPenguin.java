0a91ff0022efd8d780471a84c9a121697840c683
KebanFiru <hbagic@makersan.com>
2025-12-11 00:18:24 +0300

    Emperor penguin 's concrete methods are added

--- Year #2/First Half/CENG 211/Sliding Penguins Puzzle Game App/src/penguins/EmporerPenguin.java
+++ Year #2/First Half/CENG 211/Sliding Penguins Puzzle Game App/src/penguins/EmporerPenguin.java
@@ -2,6 +2,19 @@ package penguins;
 
 public class EmporerPenguin extends Penguin{
 
+    public EmporerPenguin(String name){
 
+        super(name);
+    }
+
+    @Override
+    public void slide(direction Direction){
+
+    }
+
+    @Override
+    public void useSpecialAbility(String direction, int value){
+
+    }
 
 }
