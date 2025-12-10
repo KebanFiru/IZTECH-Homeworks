493b4f75b47772372b572691efecd3ece022d631
KebanFiru <hbagic@makersan.com>
2025-12-11 00:59:20 +0300

    Rockchopper penguin is added

--- Year #2/First Half/CENG 211/Sliding Penguins Puzzle Game App/src/penguins/RockhopperPenguin.java
+++ Year #2/First Half/CENG 211/Sliding Penguins Puzzle Game App/src/penguins/RockhopperPenguin.java
@@ -1,4 +1,14 @@
 package penguins;
 
-public class RockhopperPenguin {
+public class RockhopperPenguin extends Penguin{
+
+    public EmperorPenguin(String name){
+
+        super(name,PenguinType.ROCKCHOPPER);
+    }
+
+    @Override
+    public PenguinType getType() {
+        return PenguinType.ROCKCHOPPER;
+    }
 }
