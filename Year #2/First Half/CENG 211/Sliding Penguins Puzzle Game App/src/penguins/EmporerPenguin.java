4331d969eb63a27cb9da6faaed43bd9623a00a15
KebanFiru <hbagic@makersan.com>
2025-12-11 00:30:19 +0300

    Special effect is added

--- Year #2/First Half/CENG 211/Sliding Penguins Puzzle Game App/src/penguins/EmporerPenguin.java
+++ Year #2/First Half/CENG 211/Sliding Penguins Puzzle Game App/src/penguins/EmporerPenguin.java
@@ -1,5 +1,7 @@
 package penguins;
 
+import java.util.Scanner
+
 public class EmporerPenguin extends Penguin{
 
     public EmporerPenguin(String name){
@@ -13,7 +15,41 @@ public class EmporerPenguin extends Penguin{
     }
 
     @Override
-    public void useSpecialAbility(String direction, int value){
+    public void useSpecialAbility(String direction) throws Exception{
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many squares you want to go(maximum of 3:)");
+        int squareValue = input.nextInt();
+
+        int[] coordinates = new int[2];
+        coordinates = getPosition();
+
+        if(direction.equals("Left")){
+
+            coordinates[0] = coordinates[0] - squareValue;
+            this.setPosition(coordinates[0], coordinates[1]);
+            
+        }
+        else if(direction.equals("Right")){
+
+            coordinates[0] = coordinates[0] + squareValue;
+            this.setPosition(coordinates[0], coordinates[1]);
+        }
+        else if(direction.equals("Up")){
+
+            coordinates[1] = coordinates[1] + squareValue;
+            this.setPosition(coordinates[0], coordinates[1]);
+        }
+        else if(direction.equals("Down")){
+
+            coordinates[1] = coordinates[1] - squareValue;
+            this.setPosition(coordinates[0], coordinates[1]);
+        }
+        else{
+            throw new Exception("Direction must be Left, Right, Up and Down not different");
+        }
+
 
     }
 
