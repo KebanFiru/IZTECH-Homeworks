3806ebde9513be6d740a296c5d85e9baa3eef31a
KebanFiru <hbagic@makersan.com>
2025-12-11 00:56:04 +0300

    Emperor penguin concrete class is re arrenged around IcyTerrain class

--- Year #2/First Half/CENG 211/Sliding Penguins Puzzle Game App/src/penguins/EmporerPenguin.java
+++ Year #2/First Half/CENG 211/Sliding Penguins Puzzle Game App/src/penguins/EmporerPenguin.java
@@ -1,50 +1,15 @@
 package penguins;
 
-import java.util.Scanner
 
-public class EmporerPenguin extends Penguin{
+public class EmperorPenguin extends Penguin{
 
-    public EmporerPenguin(String name){
+    public EmperorPenguin(String name){
 
-        super(name);
+        super(name,PenguinType.EMPEROR);
     }
 
     @Override
-    public void useSpecialAbility(String direction) throws Exception{
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("How many squares you want to go(maximum of 3:)");
-        int squareValue = input.nextInt();
-
-        int[] coordinates = new int[2];
-        coordinates = getPosition();
-
-        if(direction.equals("Left")){
-
-            coordinates[0] = coordinates[0] - squareValue;
-            this.setPosition(coordinates[0], coordinates[1]);
-            
-        }
-        else if(direction.equals("Right")){
-
-            coordinates[0] = coordinates[0] + squareValue;
-            this.setPosition(coordinates[0], coordinates[1]);
-        }
-        else if(direction.equals("Up")){
-
-            coordinates[1] = coordinates[1] + squareValue;
-            this.setPosition(coordinates[0], coordinates[1]);
-        }
-        else if(direction.equals("Down")){
-
-            coordinates[1] = coordinates[1] - squareValue;
-            this.setPosition(coordinates[0], coordinates[1]);
-        }
-        else{
-            throw new Exception("Direction must be Left, Right, Up and Down not different");
-        }
-
-
+    public PenguinType getType() {
+        return PenguinType.EMPEROR;
     }
 }
