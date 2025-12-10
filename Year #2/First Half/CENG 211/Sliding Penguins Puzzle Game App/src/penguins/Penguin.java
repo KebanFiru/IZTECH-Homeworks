7a364b869e8088d89fc943f99cae44901c802a31
KebanFiru <hbagic@makersan.com>
2025-12-11 00:54:02 +0300

    Penguin abstract class is refined for IcyTerrain

--- Year #2/First Half/CENG 211/Sliding Penguins Puzzle Game App/src/penguins/Penguin.java
+++ Year #2/First Half/CENG 211/Sliding Penguins Puzzle Game App/src/penguins/Penguin.java
@@ -1,104 +1,89 @@
 package penguins;
 
 import food.FoodType;
-import util.ExtendedArrayList;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Penguin {
-
-    private String type;
-    private String name;
+    private PenguinType type;
+    private final String name;
     private int x;
     private int y;
-    private ExtendedArrayList<FoodType> collectedFoods;
-    private String[] moves;
-    private int wichMove;
+    private List<FoodType> collectedFoods;
     private int movesLeft;
     private boolean specialEffectUsed;
     private boolean stunned;
-    private int totalWeight;
     private boolean inGame;
 
-    public Penguin(String name){
+    public Penguin(String name, PenguinType type) {
 
+        this.name = name;
+        this.type = type;
         this.x = 0;
         this.y = 0;
-        this.collectedFoods = new ExtendedArrayList<FoodType>(0,FoodType);
-        this.moves = new String[4];
-        this.wichMove = 0;
+        this.collectedFoods = new ArrayList<>();
         this.movesLeft = 4;
         this.specialEffectUsed = false;
         this.stunned = false;
-        this.totalWeight = 0;
         this.inGame = true;
     }
 
-    public abstract String getType();
-
-    public int[] getPosition(){
-        int[] position = new int[2];
-        position[0] = x;
-        position[1] = y;
-
-        return position;
+    public PenguinType getType() {
+        return type;
+    }
+    public String getName() {
+        return name;
     }
 
-    public void setPosition(int x, int y){
-        this.x = x;
-        this.y = y;
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setPosition(int x, int y) {
+        this.x = x; this.y = y;
+    }
+    public List<FoodType> getCollectedFoods() {
+        return collectedFoods;
     }
 
-    public abstract void slide(Direction direction);
-
-    public abstract void useSpecialAbility(Direction direction);
-
-    public void addFood(FoodType food){
-
+    public void collectFood(FoodType food) {
         collectedFoods.add(food);
     }
-
-    public int getTotalWeight(){
-
-        for(FoodType food: collectedFoods){
-            totalWeight = totalWeight+food.getWeight();
+    public int getTotalFoodWeight() {
+        int totalWeight = 0;
+        for (FoodType food : collectedFoods) {
+            totalWeight += food.getWeight();
         }
-
         return totalWeight;
     }
-
-    public boolean isStunned(){
-
-        return this.stunned;
+    public boolean isStunned() {
+        return stunned;
     }
-
-    public void setStun(boolean value){
+    public void setStunned(boolean value) {
         this.stunned = value;
     }
 
-    public void move(String direction){
-
-        if(this.moves.length < 4){
-            moves[wichMove] = direction;
-            wichMove = wichMove+1;
-            movesLeft = movesLeft - 1;
-        }
-        else{
-            throw new ArrayIndexOutOfBoundsException("A penuin only can move 4 times");
-        }
-    }
-
-    public boolean isIngGame(){
-
+    public boolean isInGame() {
         return inGame;
     }
-
-    public void setInGame(boolean value){
-
+    public void setInGame(boolean value) {
         this.inGame = value;
     }
-
-    public void setSpecialEffectUsed(boolean value){
-
+    public boolean hasUsedSpecialEffect() {
+        return specialEffectUsed;
+    }
+    public void setSpecialEffectUsed(boolean value) {
         this.specialEffectUsed = value;
     }
-    
+
+    public int getMovesLeft() {
+        return movesLeft;
+    }
+    public void decrementMoves() {
+        if (movesLeft > 0){
+            movesLeft--;
+        }
+    }
 }
