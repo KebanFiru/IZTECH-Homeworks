f99add0d0f112c7a33a1defb9dbf5ed8d84aa7df
KebanFiru <143508175+KebanFiru@users.noreply.github.com>
2025-12-15 17:16:26 +0300

    ToolUser generic is implemented

--- Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/util/ToolUser.java
+++ Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/util/ToolUser.java
@@ -1,4 +1,8 @@
 package util;
 
-public class ToolUser {
+import model.tools.SpecialTool;
+
+public interface ToolUser <T extends SpecialTool>{
+
+    public void useTool(T tool, int row, int column);
 }
