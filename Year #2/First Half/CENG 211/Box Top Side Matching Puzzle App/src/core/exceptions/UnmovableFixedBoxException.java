46495163f712310f6ef05305e57c6612bd51c107
KebanFiru <143508175+KebanFiru@users.noreply.github.com>
2025-12-15 17:28:42 +0300

    UnmovableFixedBoxException is implemented

--- Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/core/exceptions/UnmovableFixedBoxException.java
+++ Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/core/exceptions/UnmovableFixedBoxException.java
@@ -1,4 +1,12 @@
 package core.exceptions;
 
-public class UnmovableFixedBoxException {
+public class UnmovableFixedBoxException extends RuntimeException {
+
+    public UnmovableFixedBoxException(){
+        super();
+    }
+    public UnmovableFixedBoxException(String message) {
+
+        super(message);
+    }
 }
