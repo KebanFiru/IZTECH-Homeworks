4af77b8a32216a04640ebf726d8c8e067de8ed5a
KebanFiru <143508175+KebanFiru@users.noreply.github.com>
2025-12-15 16:10:15 +0300

    Object... args -> int row, int column

--- Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/BoxFixer.java
+++ Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/BoxFixer.java
@@ -11,17 +11,7 @@ public class BoxFixer extends SpecialTool{
         super("BoxFixer");
     }
 
-    public void use(BoxGrid grid,Object... args){
-
-        if(args.length!=2){
-            throw new ArrayStoreException("args must be length of 2 ");
-        }
-        if(!(args[0] instanceof Integer) || !(args[1] instanceof Integer) ){
-            throw new IllegalArgumentException("args[0] and args[1] must be type of integer");
-        }
-
-        int row = (int)args[0];
-        int column = (int)args[1];
+    public void use(BoxGrid grid,int row, int column){
 
         Box box = grid.getBox(row, column);
 
