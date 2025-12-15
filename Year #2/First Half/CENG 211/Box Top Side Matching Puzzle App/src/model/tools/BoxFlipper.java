c934822e0d9abd0f015ad5af77f8aedd74ea3dcc
KebanFiru <143508175+KebanFiru@users.noreply.github.com>
2025-12-15 16:27:29 +0300

    BoxFlipper class is implemented

--- Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/BoxFlipper.java
+++ Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/BoxFlipper.java
@@ -1,10 +1,25 @@
 package model.tools;
 
+import core.BoxGrid;
+
 public class BoxFlipper extends SpecialTool{
 
     public BoxFlipper(){
         super("BoxFlipper");
     }
 
-    
+    public void use(Box box){
+
+        char[] boxSideLetters = box.getSides();
+
+        // I assume top as indexed 1 side and 3 as bottom indexed side
+
+        char bottomSide = boxSideLetters[3];
+        char topSide = box.getTopLetter();
+
+        //Implement setSide in Box
+        box.setSide(1, bottomSide);
+        box.setSide(3, topSide);
+
+    }
 }
