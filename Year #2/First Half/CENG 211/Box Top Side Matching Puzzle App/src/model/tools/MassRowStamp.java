d4c14107f0650f7592a44c707906f38901c0d1e4
KebanFiru <143508175+KebanFiru@users.noreply.github.com>
2025-12-15 16:43:38 +0300

    MassRowStamp is implemented

--- Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/MassRowStamp.java
+++ Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/MassRowStamp.java
@@ -1,4 +1,23 @@
 package model.tools;
 
-public class MassRowStamp {
+public class MassRowStamp extends SpecialTool{
+
+    public MassRowStamp(){
+        super("MassRowStamp");
+    }
+
+    public void use(BoxGrid grid, int row, int column){
+
+        char stampLetter = grid.getStampLetter();
+
+        for(int i= 0; i<grid.getColumnCount; i++){
+
+            Box box = grid.getBox(row, i);
+
+            if(!(box instanceof FixedBox)){
+
+                box.setTopLetter(stampLetter);
+            }
+        }
+    }
 }
