a16cc575d6592196156b26bbafc7a3409774341e
KebanFiru <143508175+KebanFiru@users.noreply.github.com>
2025-12-15 16:41:19 +0300

    MassColumnStamp is implemented

--- Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/MassColumnStamp.java
+++ Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/MassColumnStamp.java
@@ -1,6 +1,7 @@
 package model.tools;
 
 import core.BoxGrid;
+import model.box.FixedBox;
 
 public class MassColumnStamp extends SpecialTool{
 
@@ -9,7 +10,18 @@ public class MassColumnStamp extends SpecialTool{
         super("MassColumnStamp");
     }
 
-    public use(BoxGrid grid, int row, int column){
-        
+    public void use(BoxGrid grid, int row, int column){
+
+        char stampLetter = grid.getStampLetter();
+
+        for(int i= 0; i<grid.getRowCount; i++){
+
+            Box box = grid.getBox(i, column);
+
+            if(!(box instanceof FixedBox)){
+
+                box.setTopLetter(stampLetter);
+            }
+        }
     }
 }
