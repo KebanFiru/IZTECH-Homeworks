d1187ff08f5097863144d2224759458b4da2a665
KebanFiru <143508175+KebanFiru@users.noreply.github.com>
2025-12-15 17:09:30 +0300

    PlusShapeStamp class is implemented

--- Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/PlusShapeStamp.java
+++ Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/PlusShapeStamp.java
@@ -1,4 +1,33 @@
 package model.tools;
 
-public class PlusShapeStamp {
+import core.BoxGrid;
+import core.exceptions.BoxAlreadyFixedException;
+import model.box.Box;
+import model.box.FixedBox;
+
+public class PlusShapeStamp extends SpecialTool{
+
+    public PlusShapeStamp(){
+        super("PlusShapeStamp");
+    }
+
+    public void use(BoxGrid grid, int row, int column){
+        Box centerBox = grid.getBox(row,column);
+
+        if(centerBox instanceof FixedBox){
+            throw new BoxAlreadyFixedException("Box already fixed");
+        }
+
+        Box upBox = grid.getBox(row-1,column);
+        Box bottomBox = grid.getBox(row+1,column);
+        Box leftBox = grid.getBox(row,column-1);
+        Box rightBox = grid.getBox(row,column+1);
+
+        upBox.setTopLetter(grid.getStampLetter());
+        centerBox.setTopLetter(grid.getStampLetter());
+        bottomBox.setTopLetter(grid.getStampLetter());
+        leftBox.setTopLetter(grid.getStampLetter());
+        rightBox.setTopLetter(grid.getStampLetter());
+
+    }
 }
