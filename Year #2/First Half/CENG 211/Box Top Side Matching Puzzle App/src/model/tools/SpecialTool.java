9df1ca43552a712da8ad94cbfe024b0db7d32e48
mertinkos <mertturkilli01@gmail.com>
2025-12-17 20:28:08 +0300

    Revert "SpecialTTool abstract class is implementd"

--- Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/SpecialTool.java
+++ Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/SpecialTool.java
@@ -1,21 +1,4 @@
 package model.tools;
 
-import core.BoxGrid;
-import model.box.Box;
-
-public abstract class SpecialTool {
-
-    private String name;
-
-    public SpecialTool(String name){
-
-        this.name = name;
-    }
-
-    public String getName(){
-
-        return name;
-    }
-
-    public abstract void use(BoxGrid grid, int row, int column);
+public class SpecialTool {
 }
