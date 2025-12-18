7c9df509243f8d02442b8a13bda8879211d7fc9d
KebanFiru <143508175+KebanFiru@users.noreply.github.com>
2025-12-18 20:30:50 +0300

    Added forgetten things

--- Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/MassRowStamp.java
+++ Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/MassRowStamp.java
@@ -1,16 +1,18 @@
 package model.tools;
 
+import model.box.Box;
+
 public class MassRowStamp extends SpecialTool{
 
     public MassRowStamp(){
         super("MassRowStamp");
     }
 
-    public void use(BoxGrid grid, int row, int column){
+    public void useTool(BoxGrid grid, int row){
 
         char stampLetter = grid.getStampLetter();
 
-        for(int i= 0; i<grid.getColumnCount; i++){
+        for(int i= 0; i<grid.getColumnCount(); i++){
 
             Box box = grid.getBox(row, i);
 
