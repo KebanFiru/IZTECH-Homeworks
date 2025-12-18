f84ed03f83c677864ceda24c06f446f93dd50149
KebanFiru <143508175+KebanFiru@users.noreply.github.com>
2025-12-18 16:00:34 +0300

    Logic is updated according to homework

--- Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/PlusShapeStamp.java
+++ Year #2/First Half/CENG 211/Box Top Side Matching Puzzle App/src/model/tools/PlusShapeStamp.java
@@ -14,20 +14,19 @@ public class PlusShapeStamp extends SpecialTool{
     public void use(BoxGrid grid, int row, int column){
         Box centerBox = grid.getBox(row,column);
 
-        if(centerBox instanceof FixedBox){
-            throw new BoxAlreadyFixedException("Box already fixed");
+        if(!(centerBox instanceof FixedBox)){
+
+            Box upBox = grid.getBox(row-1,column);
+            Box bottomBox = grid.getBox(row+1,column);
+            Box leftBox = grid.getBox(row,column-1);
+            Box rightBox = grid.getBox(row,column+1);
+
+            upBox.setTopLetter(grid.getStampLetter());
+            centerBox.setTopLetter(grid.getStampLetter());
+            bottomBox.setTopLetter(grid.getStampLetter());
+            leftBox.setTopLetter(grid.getStampLetter());
+            rightBox.setTopLetter(grid.getStampLetter());
+            
         }
-
-        Box upBox = grid.getBox(row-1,column);
-        Box bottomBox = grid.getBox(row+1,column);
-        Box leftBox = grid.getBox(row,column-1);
-        Box rightBox = grid.getBox(row,column+1);
-
-        upBox.setTopLetter(grid.getStampLetter());
-        centerBox.setTopLetter(grid.getStampLetter());
-        bottomBox.setTopLetter(grid.getStampLetter());
-        leftBox.setTopLetter(grid.getStampLetter());
-        rightBox.setTopLetter(grid.getStampLetter());
-
     }
 }
